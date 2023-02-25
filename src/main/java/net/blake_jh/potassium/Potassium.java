4847af9c124d79ee300036b37b70bf3756ea1c10
BlakeJH <blakejenkinshanks@gmail.com>
2023-02-24 20:13:26 -0700

    Finally created a github for my project

---
+++ src/main/java/net/blake_jh/potassium/Potassium.java
@@ -0,0 +1,16 @@
+package net.blake_jh.potassium;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Potassium implements ModInitializer {
+
+	public static final String MOD_ID = "potassium";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/blake_jh/potassium/PotassiumModClient.java
@@ -0,0 +1,10 @@
+package net.blake_jh.potassium;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PotassiumModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
